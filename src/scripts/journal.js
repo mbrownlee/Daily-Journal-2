c9b91f406c29025d2bb38f55a78be061aaa9b278
mbrownlee <michelle@orbwave.com>
2020-04-21 21:25:02 -0500

    added scripts file

---
+++ src/scripts/journal.js
@@ -0,0 +1,14 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const journalEntry = {
+    date: "date",
+    topic: "topic", 
+    entry: "yada yada yada",
+    mood: "pit of despair"
+}
+
+const entries = []
+
+entries.push(journalEntry)
