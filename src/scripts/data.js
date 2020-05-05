9da4de6906a8a390c103eaa49cceff5ff84fb2a7
mbrownlee <michelle@orbwave.com>
2020-05-05 16:44:25 -0500

    can create new entry

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,12 +1,17 @@
 const API = {
-    getJournalEntries () {
-        return fetch("http://localhost:3000/entries") // Fetch from the API
-        .then(entries => entries.json()) }// Parse as JSON
-    }
+  getJournalEntries() {
+    return fetch("http://localhost:3000/entries") // Fetch from the API
+      .then((entries) => entries.json());
+  },
+  createNewEntry(newJournalEntryObj) {
+    fetch("http://localhost:3000/entries", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJournalEntryObj),
+    });
+  },
+};
 
-
-    
-export default  API
-
-    
-    
+export default API;
---
+++ src/scripts/entryCreation.js
@@ -0,0 +1,8 @@
+const newJournalEntry = (date, topic, entry, mood) => ({
+	date: date,
+	topic: topic,
+	entry: entry,
+	mood: mood
+});
+
+export default newJournalEntry
